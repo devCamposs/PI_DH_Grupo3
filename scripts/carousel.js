b4662f4591b58db775cf24811246c0143635d192
Luciana <lucyfierceit@gmail.com>
2022-09-09 19:43:40 -0300

    dropdown

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -1,6 +1,16 @@
-function button (){
-    document.getElementById("bass").src="./img/carouselHomeBaixo2.png"
-}
+// function button (){
+//     document.getElementById("bass").src="./img/carouselHomeBaixo2.png"
+// }
 var intervalId = window.setInterval(function(){
-    button();
+    changeImg();
   }, 3000);
+
+const carousel = ["./img/carouselHomeBaixo1.jpg", "./img/carouselHomeBaixo2.png", "./img/carouselHomeBaixo3.png"];
+let index = 0;
+
+changeImg = () => {
+    if (index >= carousel.length) index = 0;
+
+    document.getElementById("bass").src = carousel[index];
+    index++;
+};
