23696d473c3ee9856668a0c9bd61d009440c0c5b
Your Name <you@example.com>
2022-10-02 18:53:29 -0300

    oraganizado pastas modelo MVC

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
-const express = requipre('express')
+const express = require('express')
 const app = express()
+const path = require('path')
+
+app.set('view engine', 'ejs')
 
 app.use(express.static(path.join(__dirname)))
-app.listen(3000, ()=> {console})
+app.listen(3000, ()=> {console.log('Servidor rodando na porta 3000')})
