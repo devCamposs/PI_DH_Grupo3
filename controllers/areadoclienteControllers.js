ae30a5e8732a264a9e6be7d9ee5862c24f58ec9e
Lucyfierce <109184634+Lucyfierce@users.noreply.github.com>
2022-10-09 21:36:40 -0300

    comitando para poder tricar de branch nada novo (#52)

--- controllers/areadoclienteControllers.js
+++ controllers/areadoclienteControllers.js
@@ -1,8 +1,23 @@
 const path = require('path')
 
 const areadoclienteControllers = {
+    viewForm: (req, res) =>{
+        return res.render('areadoclienteLogin')
+    },
+
+    salvarForm: (req, res) =>{
+        let {email, password} = req.body;
+
+        // console.log(req.body)
+
+        // res.send("") //o que deve aparecer para o cliente apos login?
+        res.redirect('/')
+    },
+
     index: (req, res) => {
         res.render('areadoclientelogin')
-    }
+    },
+
+
 }
 module.exports = areadoclienteControllers
--- routes/areadocliente.js
+++ routes/areadocliente.js
@@ -4,4 +4,8 @@ const router = express.Router()
 const areadoclienteControllers = require('../controllers/areadoclienteControllers')
 router.get('/', areadoclienteControllers.index)
 
+router.get('/areadoclientelogin/criar', areadoclienteControllers.viewForm)
+router.post('/areadoclientelogin/criar', areadoclienteControllers.salvarForm)
+
+
 module.exports = router
