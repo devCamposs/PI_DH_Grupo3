d0de754c4f717bad90ca3a94af02dec0e3fb0795
Lucyfierce <109184634+Lucyfierce@users.noreply.github.com>
2022-10-19 19:56:00 -0300

    barraPesquisa (#59)

--- public/js/pesquisa.js
+++ public/js/pesquisa.js
@@ -2,10 +2,23 @@ var btn = document.querySelector('#show-or-hide')
 var search = document.querySelector('.search')
 
 btn.addEventListener('click', () =>{
-    if(search.style.display === 'block'){
-        search.style.display = 'none'
-    } else {
-        search.style.display = 'block'
-    }
+    pesquisaToggle()  
 })
 
+function on() {
+    document.getElementById("overlay").style.display = "block";
+  }
+  
+  function off() {
+    document.getElementById("overlay").style.display = "none";
+  }
+
+  pesquisaToggle = () => {
+    if(search.style.display === 'flex'){
+        search.style.display = 'none';
+        off()
+    } else {
+        search.style.display = 'flex';
+        on();
+    }
+  } 
