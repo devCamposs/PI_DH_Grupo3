ad6b87cebc2c946441684aa377c4e80984354799
Lucyfierce <109184634+Lucyfierce@users.noreply.github.com>
2022-10-24 22:47:19 -0300

    cadastroAreacliente (#64)

--- controllers/areadoclienteControllers.js
+++ controllers/areadoclienteControllers.js
@@ -1,4 +1,9 @@
-//const path = require('path')
+const fs = require('fs');
+const path = require('path');
+const {validationResult} = require('express-validator')
+
+const usuariosFilePath = path.join(__dirname, '../data/cadastroClientes.json');
+const usuarios = JSON.parse(fs.readFileSync(usuariosFilePath, 'utf-8'));
 
 const areadoclienteControllers = {
     index: (req, res) => {
@@ -10,11 +15,25 @@ const areadoclienteControllers = {
     },
     
     salvarForm: (req, res) =>{
-        
-        console.log('login', req.body)
-        res.redirect('/')    
+        let errors = validationResult(req)
+        if (!errors.isEmpty()){
+            return res.render('areadoclientelogin', {erros: errors.errors})
+        }
+
+        let usuario = {
+            id: usuarios[usuarios.length -1].id + 1,
+            usuarios: usuarios,
+            // req.body,
+        }
+        usuarios.push(novoUsuario)
+        fs.writeFileSync(usuariosFilePath, JSON.stringify(usuarios, null, ' '))
+        res.redirect('/')
     },
-    
-    
 }
+        // console.log('login', req.body)
+        // res.redirect('/')    
+
+    
+    
+
 module.exports = areadoclienteControllers
